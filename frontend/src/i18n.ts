b473834298b64a64756d0669183bc6d67ac5c3c2
Jose Villaverde <jvillaverde@gmail.com>
2026-05-29 11:57:32 +0000

    Register Spanish locale in frontend

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -41,3 +41,37 @@ if (typeof window !== "undefined") {
 }
 
 export default i18n;
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import commonEn from "../locales/en/common.json";
+import commonDe from "../locales/de/common.json";
+import commonEs from "../locales/es/common.json";
+
+const resources = {
+  en: {
+    common: commonEn,
+  },
+  de: {
+    common: commonDe,
+  },
+  es: {
+    common: commonEs,
+  },
+};
+
+void i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  fallbackLng: "en",
+  defaultNS: "common",
+  ns: ["common"],
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+i18n.on("languageChanged", (language) => {
+  document.documentElement.lang = language;
+});
+
+export default i18n;
